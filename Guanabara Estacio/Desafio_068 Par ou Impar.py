39b1c9e5e643997a3f8ee57733b12ca4a2cf45b9
rodrigocra1 <rodrigo.cr.aguiar@gmail.com>
2025-09-27 10:12:10 -0400

    Refactor Par ou Impar game logic: Update user selection input, enhance game logic, and remove unused functions.

--- Guanabara Estacio/Desafio_068 Par ou Impar.py
+++ Guanabara Estacio/Desafio_068 Par ou Impar.py
@@ -3,37 +3,46 @@ import random
 
 
 def selectPouI():
-    userSelection = int(input("Selecione: \n1 - Par\n2 - Impar\nSua escolha: "))
+    userSelection = int(input("Selecione: \n0 - Par\n1 - Impar\nSua escolha: "))
     while True:
-        if userSelection == 1:
-            return 'Par'
+        if userSelection == 0:
+            return 0
             break
-        elif userSelection == 2:
-            return 'Impar'
+        elif userSelection == 1:
+            return 1
             break
         else:
             print("Digite uma opção válida")
+            userSelection = int(input("Selecione: \n0 - Par\n1 - Impar\nSua escolha: "))
 
-def PCTurn():
-    pcTurnNumber = int(random.randint(1,10))
-    if pcTurnNumber %2 == 0:
-        return 'Par'
-    else:
-        return 'Impar'
+    
 
-def UsrTURN():
-    userTurnNumber = int(input("Digite um número de 1 a 10: "))
-    if userTurnNumber %2 == 0:
-        return 'Par'
-    else:
-        return 'Impar'
+
+
+'''Criar uma variavel gameResult com o gameSoma %2 '''
+'''Criar o gameSoma antes do gameResult para usar no print fiinal'''
+
+def gameLogic():
+    gameSoma = pcTurnNumber + userTurnNumber ##verificar nomes das variaveis
+    if bet == 0 and gameSoma %2 == 0: ##set userBet par = 0
+        print(f"O computador escolheu {pcTurnNumber}, o resultado {gameSoma} é par!\nVocê ganhou!")
+    elif bet == 1 and gameSoma %2 == 1:
+        print(f"O computador escolheu {pcTurnNumber}, o resultado {gameSoma} é ímpar!\nVocê ganhou!")
+    elif bet == 0 and gameSoma %2 == 1:
+        print(f"O computador escolheu {pcTurnNumber}, o resultado {gameSoma} é ímpar!\nVocê perdeu!")
+    elif bet == 1 and gameSoma %2 == 0:
+        print(f"O computador escolheu {pcTurnNumber}, o resultado {gameSoma} é par!\nVocê perdeu!")
+
 
 
 bet = selectPouI()
-pcTurn = PCTurn()
-userTUrn = UsrTURN()
+pcTurnNumber = int(random.randint(1,10))
+userTurnNumber = int(input("Digite um número de 1 a 10: "))
+while True:
+    if userTurnNumber > 10:
+        print("Digite uma opção válida")
+        userTurnNumber = int(input("Digite um número de 1 a 10: "))
+    else:
+        break
 
-print(bet)
-print(pcTurn)
-print(userTUrn)
-
+gameLogic()
